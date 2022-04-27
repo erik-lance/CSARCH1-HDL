0b95e66c830b184b219b5be2342a1e13d1f4e346
erik-lance <eriklance@gmail.com>
2022-04-27 16:28:52 +0800

    Added proper function

--- tioe1.v
+++ tioe1.v
@@ -1,6 +1,14 @@
-//test
-module tioe1(A, B, C, D);
+// Tiongquico, Erik Lance L. (S11)
+/*
+    Boolean Function
+    F =(AB' + A'B) (C+D')
 
+*/
+module tioe1(F, A, B, C, D);
+    output F;
+    input A, B, C, D;
+
+    assign F = ( (A && !B) || (!A && B) ) && (C && !D);
 
 
 endmodule
