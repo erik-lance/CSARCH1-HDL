85b2ca4b0b3a84c44ac020f52572abb99ef0c064
erik-lance <eriklance@gmail.com>
2022-04-28 18:43:45 +0800

    Used control transfer construct, displayed necessary info
    Control Transfer Construct used for repeating automatically. Also displays 0000 at the end as noted by instructions.
    
    Displayed name, boolean function, and moddel used for the project.

--- tioe1_tb.v
+++ tioe1_tb.v
@@ -1,33 +1,22 @@
-//Test bench
-`include "tioe1.v"
-
+// Tiongquico, Erik Lance L. (S11)
 module tioe1_tb();
-    reg t_A, t_B, t_C, t_D;
+    reg [3:0] t_input;
     wire t_F;
 
-    tioe1 dut(t_F, t_A, t_B, t_C, t_D);
+    tioe1 dut(t_F, t_input[3], t_input[2], t_input[1], t_input[0]);
 
     initial begin
-        t_A = 0;
-        t_B = 0;
-        t_C = 0;
-        t_D = 0;
-
-        #10 //after 10 ns
-        t_A = 0;
-        t_B = 0;
-        t_C = 0;
-        t_D = 1;
-
-        #10
-        t_A = 0;
-        t_B = 0;
-        t_C = 1;
-        t_D = 0;
+        t_input = 4'b0000;
+        repeat(15)
+            #10 t_input = t_input + 4'b0001;
+        #10 t_input = 4'b0000;
     end
 
     initial begin
-        $monitor("time = %0d ", $time, "A = %b B = %b C = %b D = %b output_F = %b", t_A, t_B, t_C, t_D, t_F);
+        $display("Written by Erik Lance Tiongquico - S11");
+        $display("(AB' + A'B) (C+D')");
+        $display("Verilog Dataflow Model");
+        $monitor("time = %0d ", $time, "A = %b B = %b C = %b D = %b output_F = %b", t_input[3], t_input[2], t_input[1], t_input[0], t_F);
         $dumpfile("tioe1.vcd");
         $dumpvars();
     end
