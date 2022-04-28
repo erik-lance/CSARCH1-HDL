bcf295197fdd5544505f107e9c76f496ac55d613
erik-lance <eriklance@gmail.com>
2022-04-28 18:42:33 +0800

    Changed to Verilog Dataflow format

--- tioe1.v
+++ tioe1.v
@@ -8,7 +8,8 @@ module tioe1(F, A, B, C, D);
     output F;
     input A, B, C, D;
 
-    assign F = ( (A && !B) || (!A && B) ) && (C && !D);
+    assign F = (A&~B | ~A&B) & (C | ~D);
+    //assign F = ( (A && !B) || (!A && B) ) && (C && !D);
 
 
 endmodule
